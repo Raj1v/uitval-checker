190bd781a0306da536c2464fd28768e95aac3de2
kvtul_000 <kvtul_000@192.168.192.13>
2013-05-14 19:09:16 +0200

    Begin html getter gemaakt

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class UitvalChecker {
 
 	/**
@@ -7,6 +12,14 @@ public class UitvalChecker {
 		try
 		{
 		System.out.println("UitvalChecker V1.0");
+		URL infoweb = new URL("http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=5&id='CG3A'");
+		BufferedReader bf = new BufferedReader(new InputStreamReader(infoweb.openStream()));
+		String html = null;
+		String line = null;
+		while((line = bf.readLine())!=null)
+		{
+			html = html + "\n" + line;
+		}
 		}
 		catch(Exception e)
 		{
