1ae7b1a1e16bf8d39566c5e7b6cfeeab8317bf70
Raj1v <rajivmanichand@gmail.com>
2013-05-14 19:19:58 +0300

    Update UitvalChecker.java

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -6,5 +6,10 @@ public class UitvalChecker {
 	public static void main(String[] args) {
 		System.out.println("Hoi ik ben klein");
 	}
+	
+	public static void testFunction(){
+		System.out.println("Dit is een test");
+		
+	}
 
 }
