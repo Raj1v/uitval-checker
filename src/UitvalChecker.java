41f2d4539c799111d97478cd6c6472fb1e79de47
kvtul_000 <kvtul_000@192.168.192.13>
2013-08-27 13:40:32 +0200

    import java.io.IOException; import java.util.ArrayList; import java.util.List; import org.jsoup.Jsoup; import org.jsoup.nodes.Document; import org.jsoup.nodes.Element; import org.jsoup.select.Elements; public class UitvalChecker { public static void main(String[] args) { int leerlingNummer = 100616; String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id=" + leerlingNummer; Document infoweb = null; try { infoweb = Jsoup.connect(url).get(); } catch (Exception e) { System.out.println("Link not found!"); } infoweb.getElementsByClass("vrij").html("Vrij"); Elements uren = infoweb.select(".oneven > td,.even > td"); int c = 1; for (Element a : uren) { if (c == 5) { c = 1; } if (c == 1) { System.out.println(a.text()); } c++; } } }

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -7,24 +7,29 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class UitvalChecker {
-public static void main(String[] args)
-{
-int leerlingNummer = 100616;
-String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id=" + leerlingNummer;
-Document infoweb = null;
-try {
-	infoweb = Jsoup.connect(url).get();
-} catch (Exception e) {
-	System.out.println("Link not found!");
-}
-String[][] hours = new String[5][9];
-infoweb.getElementsByClass("vrij").html("Vrij");
-Elements oneven = infoweb.select(".oneven > td,.even > td");
-for(Element a : oneven)
-{
-System.out.println(a.text());
-}
-}
+	public static void main(String[] args) {
+		int leerlingNummer = 100616;
+		String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id="
+				+ leerlingNummer;
+		Document infoweb = null;
+		try {
+			infoweb = Jsoup.connect(url).get();
+		} catch (Exception e) {
+			System.out.println("Link not found!");
+		}
+		infoweb.getElementsByClass("vrij").html("Vrij");
+		Elements uren = infoweb.select(".oneven > td,.even > td");
+		int c = 1;
+		
+		for (Element a : uren) {
+			if (c == 5) {
+				c = 1;
+			}
+			if (c == 1) {
+				System.out.println(a.text());
+			}
+			c++;
+		}
+	}
 }
