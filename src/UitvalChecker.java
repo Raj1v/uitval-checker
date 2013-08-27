20b05527f20d461101e40e0d31f3463b6f8dfbd6
Rajiv Manichand <rajivmanichand@gmail.com>
2013-08-27 21:34:15 +0200

    Verbeteringen in de wijze hoe het rooster wordt gelezen.

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -9,7 +9,7 @@ import org.jsoup.select.Elements;
 
 public class UitvalChecker {
 	public static void main(String[] args) {
-		int leerlingNummer = 100616;
+		int leerlingNummer = 100463;
 		String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id="
 				+ leerlingNummer;
 		Document infoweb = null;
@@ -19,8 +19,35 @@ public class UitvalChecker {
 			System.out.println("Link not found!");
 		}
 		infoweb.getElementsByClass("vrij").html("Vrij");
-		Elements uren = infoweb.select(".oneven > td,.even > td");
-		int c = 1;
+		Elements uren = infoweb.select("tr.oneven > td");
+		//System.out.println(uren.size());
+		
+		Element[] maandag = new Element[9];
+		Element[] dinsdag = new Element[9];
+		Element[] woensdag = new Element[9];
+		Element[] donderdag = new Element[9];
+		Element[] vrijdag = new Element[9];
+		
+		int dag = 1;
+		
+		for(Element uur : uren){
+			switch(dag){
+				case 1:
+					
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+			}
+			System.out.println(uur.text());
+		}
+		
+		/*int c = 1;
 		
 		for (Element a : uren) {
 			if (c == 5) {
@@ -30,6 +57,7 @@ public class UitvalChecker {
 				System.out.println(a.text());
 			}
 			c++;
-		}
+		}*/
+		
 	}
 }
