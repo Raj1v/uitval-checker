cf0b7294cddb225d699ae8e3a9978714ecddda6f
Rajiv <rajivmanichand@gmail.com>
2013-08-28 14:01:40 +0200

    Nutteloze imports weg

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
