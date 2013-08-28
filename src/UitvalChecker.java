c6fcab616cb7822a8cc28a128382fa7347691700
Rajiv <rajivmanichand@gmail.com>
2013-08-28 13:35:30 +0200

    Code is nu meer overzichtelijk

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -8,7 +8,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class UitvalChecker {
+	
+	static String[] maandag = new String[9];
+	static String[] dinsdag = new String[9];
+	static String[] woensdag = new String[9];
+	static String[] donderdag = new String[9];
+	static String[] vrijdag = new String[9];
+	
 	public static void main(String[] args) {
+		
+		LeesRooster();
+		
+		for(String e : maandag){
+			System.out.println(e);
+		}
+		
+		
+	}
+
+	public static void LeesRooster(){
 		int leerlingNummer = 100463;
 		String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id="
 				+ leerlingNummer;
@@ -22,11 +40,6 @@ public class UitvalChecker {
 		Elements uren = infoweb.select("tr.oneven > td , tr.even > td");
 		//System.out.println(uren.text());
 		
-		String[] maandag = new String[9];
-		String[] dinsdag = new String[9];
-		String[] woensdag = new String[9];
-		String[] donderdag = new String[9];
-		String[] vrijdag = new String[9];
 		
 		int dag = 1;
 		int index = 0;
@@ -59,20 +72,6 @@ public class UitvalChecker {
 			}
 			
 		}
-		for(String e : woensdag){
-			System.out.println(e);
-		}
-		/*int c = 1;
-		
-		for (Element a : uren) {
-			if (c == 5) {
-				c = 1;
-			}
-			if (c == 1) {
-				System.out.println(a.text());
-			}
-			c++;
-		}*/
 		
 	}
 }
