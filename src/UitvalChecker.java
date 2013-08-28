3d58191ac4dc55d1b204a26c303ef6300c793e60
Administrator <Administrator@145.120.121.9>
2013-08-28 14:26:53 +0200

    Uitval waarschuwing Na 5 uur volgende dag

--- src/UitvalChecker.java
+++ src/UitvalChecker.java
@@ -23,9 +23,18 @@ public class UitvalChecker {
 		DateFormat dateFormat = new SimpleDateFormat("u");
 		Calendar cal = Calendar.getInstance();
 		int day = Integer.parseInt(dateFormat.format(cal.getTime()));
-		
+		int uur = 0;
+		if(uur > 4){
+			day++;
+		}
 		switch(day){
-		case 1:
+		case 6:
+			day = 1;
+		case 7:
+			day = 1;
+		case 8:
+			day = 1;
+		case 1:			
 			break;
 		case 2:
 			break;
@@ -35,13 +44,10 @@ public class UitvalChecker {
 			break;
 		case 5:
 			break;
-		case 6:
-			break;
-		case 7:
-			break;
 		}
 		
-		for(String e : maandag){
+		
+		for(String e : woensdag){
 			System.out.println(e);
 		}
 		
@@ -49,7 +55,8 @@ public class UitvalChecker {
 	}
 
 	public static void LeesRooster(){
-		int leerlingNummer = 100616;
+		int leerlingNummer = 100463;
+		//Rajiv: 462, Kasper 616
 		String url = "http://www.cygnusgymnasium.nl/ftp_cg/roosters/infoweb/index.php?ref=2&id="
 				+ leerlingNummer;
 		Document infoweb = null;
@@ -59,6 +66,7 @@ public class UitvalChecker {
 			System.out.println("Link not found!");
 		}
 		infoweb.getElementsByClass("vrij").html("Vrij");
+		infoweb.getElementsByClass("vervallen").html("Uitval!");
 		Elements uren = infoweb.select("tr.oneven > td , tr.even > td");
 		//System.out.println(uren.text());
 		
